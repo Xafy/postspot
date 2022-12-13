fdf6ee3adcddd715c01894297ee24e18117c60aa
Xafy <57871519+Xafy@users.noreply.github.com>
2022-12-13 07:45:43 +0200

    Adding the main header

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom
 
 import Users  from './users/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={<Users/>}/>
-        <Route path='/places/new' element={<NewPlace/>}/>
-        <Route path='*' element={<Navigate replace to="/" />}/>
-      </Routes>
+      <MainNavigation/>
+      <main>
+        <Routes>
+          <Route path='/' element={<Users/>}/>
+          <Route path='/places/new' element={<NewPlace/>}/>
+          <Route path='*' element={<Navigate replace to="/" />}/>
+        </Routes>
+      </main>
     </Router>
   );
 }
