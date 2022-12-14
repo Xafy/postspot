374a27032ea9334d540b566b47af2d800307f535
Xafy <57871519+Xafy@users.noreply.github.com>
2022-12-14 11:42:33 +0200

    creating the user places spage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom
 import Users  from './users/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import UsersPlaces from './places/pages/UsersPlaces';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <main>
         <Routes>
           <Route path='/' element={<Users/>}/>
+          <Route path='/:userId/places'><UsersPlaces/></Route>
           <Route path='/places/new' element={<NewPlace/>}/>
           <Route path='*' element={<Navigate replace to="/" />}/>
         </Routes>
