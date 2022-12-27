6faacda20cb71cd72c8de873e3bc37e0a454fe08
Xafy <57871519+Xafy@users.noreply.github.com>
2022-12-27 04:21:45 +0200

    Add Rgister form to auth page

--- src/users/pages/Auth.js
+++ src/users/pages/Auth.js
@@ -1,7 +1,71 @@
 import React from "react";
 
-const Auth = props =>{
+import useForm from "../../shared/hooks/form-hook";
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import Card from "../../shared/components/UIElements/Card"
+import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/util/validators";
 
+import './Auth.css'
+
+const Auth = () =>{
+    const [formState, inputHandler] = useForm(
+        { 
+            username:{
+                value:'',
+                isValid : false
+            },
+            email:{
+                value : '',
+                isValid : false
+            },
+            password:{
+                value : '',
+                isValid : false
+            }
+        }
+        , false)
+
+        const authSubmitHandler = event =>{
+            event.preventDefault();
+            console.log(formState)
+        }
+
+    return(
+        <Card className="authentication">
+            <h2>Login required</h2>
+        <form onSubmit={authSubmitHandler}>
+            <Input
+            id='usernsme'
+            element='input'
+            type='text'
+            label='Username'
+            errorText='Please Enter a valid username '
+            onInput ={inputHandler}
+            validators={[VALIDATOR_REQUIRE()]}/>
+            <Input
+            id='email'
+            element='input'
+            type='email'
+            label ="E-Mail"
+            placeholder='example@mail.com'
+            errorText='Please Enter a valid '
+            onInput ={inputHandler}
+            validators={[VALIDATOR_EMAIL()]}/>            
+            <Input
+            id='password'
+            element='input'
+            type='password'
+            label='Password'
+            errorText='Please Enter a valid password'
+            onInput ={inputHandler}
+            validators={[VALIDATOR_MINLENGTH(6)]}/>
+            <Button type="submit" disabled={!formState.isValid}>Register</Button>
+        </form>
+        <Button inverse onClick={switchModeHandler} >Switch to login</Button>
+        </Card>
+
+    )
 };
 
 export default Auth;
